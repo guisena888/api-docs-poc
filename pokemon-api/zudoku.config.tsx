8a59fed2de3597ad63e7e7ea3bf269aad20c1fe8
Guilherme  Sena <guisena@Guilhermes-MacBook-Pro.local>
2025-07-17 17:38:04 -0300

    initial docs

---
+++ pokemon-api/zudoku.config.tsx
@@ -0,0 +1,61 @@
+import type { ZudokuConfig } from "zudoku";
+
+const config: ZudokuConfig = {
+  site: {
+    logo: {
+      src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
+      alt: "Zudoku",
+      width: "130px",
+    },
+  },
+  navigation: [
+    {
+      type: "category",
+      label: "Documentation",
+      items: [
+        "/introduction",
+        "/endpoints",
+        "/examples",
+        {
+          type: "link",
+          icon: "folder-cog",
+          badge: {
+            label: "New",
+            color: "purple",
+          },
+          label: "API Reference",
+          to: "/api",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Useful Links",
+      collapsible: false,
+      icon: "link",
+      items: [
+        {
+          type: "link",
+          icon: "book",
+          label: "Zudoku Docs",
+          to: "https://zudoku.dev/docs/",
+        },
+      ],
+    },
+    {
+      type: "link",
+      to: "/api",
+      label: "API Reference",
+    },
+  ],
+  redirects: [{ from: "/", to: "/introduction" }],
+  apis: [
+    {
+      type: "file",
+      input: "./apis/openapi.yaml",
+      path: "/api",
+    },
+  ],
+};
+
+export default config;
