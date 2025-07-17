73e2e22ea62c6516c6598cd240c17f28f106170f
Guilherme  Sena <guisena@Guilhermes-MacBook-Pro.local>
2025-07-17 17:50:31 -0300

    initial docs

--- pokemon-api/zudoku.config.tsx
+++ pokemon-api/zudoku.config.tsx
@@ -1,7 +1,7 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
-  basePath: "/api-docs-poc",
+  basePath: "/api-docs-poc/",
   site: {
     logo: {
       src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
